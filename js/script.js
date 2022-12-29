dc77028508c373b7b09420d9f3287e13d4145f78
fred <ugodaycnzass1995@gmail.com>
2022-12-29 23:24:34 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ $(() => {
           smooth="true"
           type="nft"
           data-mark=${el.restaurant_id[0]}
-          url="http://185.211.170.2:5000/${el.marker}"
+          url="https://foodeat2.onrender.com/${el.marker}"
           emitevents="true"
         ></a-nft>
           `;
