4f95996af8fa905f18a7d333a91658c00dac1365
fred <ugodaycnzass1995@gmail.com>
2022-12-29 23:39:53 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -3,16 +3,16 @@ $(() => {
 //   .then((response) => response.json())
 //   .then((data) => {
 //    console.log(data)
-   [{marker:'kfc', restaurant_id:'3'},{marker:'mac', restaurant_id:'12'}].forEach(el => {
-      $(".scene")[0].innerHTML += `
-          <a-nft
-          smooth="true"
-          type="nft"
-          url="./output/${el.marker}"
-          emitevents="true"
-        ></a-nft>
-          `;
-    });
+   // [{marker:'kfc', restaurant_id:'3'},{marker:'mac', restaurant_id:'12'}].forEach(el => {
+   //    $(".scene")[0].innerHTML += `
+   //        <a-nft
+   //        smooth="true"
+   //        type="nft"
+   //        url="./output/${el.marker}"
+   //        emitevents="true"
+   //      ></a-nft>
+   //        `;
+   //  });
 
     $("a-nft").on("markerFound", (event) => {
       alert('fred')
