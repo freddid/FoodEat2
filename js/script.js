ca831e37035d5313fbcecfaddc5f9d3e5902b525
fred <ugodaycnzass1995@gmail.com>
2022-12-29 19:20:42 +0300

    fix

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(() => {
+   fetch('http://185.211.170.2:5000/api/markers')
+  .then((response) => response.json())
+  .then((data) => {
+   console.log(data)
+   data.forEach(el => {
+      $(".scene")[0].innerHTML += `
+          <a-nft
+          smooth="true"
+          type="nft"
+          data-mark=${el.restaurant_id[0]}
+          url="http://185.211.170.2:5000/${el.marker}"
+          emitevents="true"
+        ></a-nft>
+          `;
+    });
+
+    $("a-nft").on("markerFound", (event) => {
+      alert('fred')
+   })
+});
+});
